f31324486f36c2b60cc59fdf99a47fa425138c4a
sdfields <spencerdfields@gmail.com>
2023-01-08 15:57:03 -0600

    added jest

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
 // Packages needed for this application
 
-const inquirer = require("inquirer");
-const fs = require("fs");
+const inquirer = require(`inquirer`);
+const fs = require(`fs`);
 
 // Array of questions for user input
 
 const subjectInfo = [
     {
-        name: "Role",
-        message: "Please insert your role.",
-        type: "input",
+        name: `Role`,
+        message: `Please select your role.`,
+        type: `checkbox`,
+        choices: [`Engineer`, `Intern`, `Manager`]
     },
     {
-        name: "Name",
-        message: "Please insert your name.",
-        type: "input",
+        name: `Name`,
+        message: `Please insert your name.`,
+        type: `input`,
     },
     {
-        name: "ID",
-        message: "Please insert your ID.",
-        type: "input",
+        name: `ID`,
+        message: `Please insert your ID.`,
+        type: `input`,
     },
     {
-        name: "Email",
-        message: "Please insert your email.",
-        type: "input",
+        name: `Email`,
+        message: `Please insert your email.`,
+        type: `input`,
     },
     {
-        name: "Office Number",
-        message: "Please insert your office number.",
-        type: "input",
+        name: `Office Number`,
+        message: `Please insert your office number.`,
+        type: `input`,
     },
     {
-        name: "GitHub",
-        message: "Please insert your GitHub.",
-        type: "input",
+        name: `GitHub`,
+        message: `Please insert your GitHub.`,
+        type: `input`,
     },
     {
-        name: "School",
-        message: "Please insert the name of your school.",
-        type: "input",
+        name: `School`,
+        message: `Please insert the name of your school.`,
+        type: `input`,
     },
 ];
 
@@ -52,10 +53,10 @@ function init() {
     inquirer
     .prompt(subjectInfo)
     .then((answers) =>
-      fs.writeFile('index.html', generateHTML(answers), (error) => {
+      fs.writeFile(`teamprofile.html`, generateHTML(answers), (error) => {
            return error 
           ? console.error(error)
-          : console.log('Team Profile uploaded successfully!')
+          : console.log(`Team Profile uploaded successfully!`)
       })
     );
 }
