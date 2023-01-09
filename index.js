06feacc935691facfc7c770eafcb9b0fd7e91360
sdfields <spencerdfields@gmail.com>
2023-01-09 11:05:07 -0600

    updated formatting and beginning index js work

--- index.js
+++ index.js
@@ -3,15 +3,20 @@
 const inquirer = require(`inquirer`);
 const fs = require(`fs`);
 
+// Access to classes
+
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+// Empty Array for Team
+
+const teamMembers = [];
+
 // Array of questions for user input
 
-const subjectInfo = [
-    {
-        name: `Role`,
-        message: `Please select your role.`,
-        type: `checkbox`,
-        choices: [`Engineer`, `Intern`, `Manager`]
-    },
+const teamInfo = [
     {
         name: `Name`,
         message: `Please insert your name.`,
@@ -28,9 +33,10 @@ const subjectInfo = [
         type: `input`,
     },
     {
-        name: `Office Number`,
-        message: `Please insert your office number.`,
-        type: `input`,
+        name: `Role`,
+        message: `Please select your role.`,
+        type: `checkbox`,
+        choices: [`Engineer`, `Intern`, `Manager`]
     },
     {
         name: `GitHub`,
@@ -42,26 +48,35 @@ const subjectInfo = [
         message: `Please insert the name of your school.`,
         type: `input`,
     },
+    {
+        name: `Office Number`,
+        message: `Please insert your office number.`,
+        type: `input`,
+    },
 ];
+
+
+
+
+
+
 
 // function generateHTML(answers) {
     
 // }
 
-// Should I write an HTML file and have the contents transcribed from index.html to myteam.html and append the users info?
-
 // Function to initialize app
-function init() {
-    inquirer
-    .prompt(subjectInfo)
-    .then((answers) =>
-      fs.writeFile(`myteam.html`, generateHTML(answers), (error) => {
-           return error 
-          ? console.error(error)
-          : console.log(`Team Profile uploaded successfully!`)
-      })
-    );
-}
+// function init() {
+//     inquirer
+//     .prompt(subjectInfo)
+//     .then((answers) =>
+//       fs.writeFile(`myteam.html`, generateHTML(answers), (error) => {
+//            return error 
+//           ? console.error(error)
+//           : console.log(`Team Profile uploaded successfully!`)
+//       })
+//     );
+// }
 
 // Function call to initialize app
-init();
+// init();
