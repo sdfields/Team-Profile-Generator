333550e28ecd1d043a5513540e4764c0125ba92b
sdfields <spencerdfields@gmail.com>
2023-01-09 19:15:52 -0600

    created barebones html and css to start

--- index.js
+++ index.js
@@ -17,99 +17,93 @@ const teamMembersData = [];
 // Array of questions for user input
 
 console.log("Hello, please insert the information as you are prompted.");
- 
+
 const questions = [
-    {
-      name: `name`,
-      message: `Please insert your name.`,
-      type: `input`,
-    },
-    {
-      name: `id`,
-      message: `Please insert your ID.`,
-      type: `input`,
-    },
-    {
-      name: `email`,
-      message: `Please insert your email.`,
-      type: `input`,
-    },
-    {
-      name: `role`,
-      message: `Please select your role.`,
-      type: `list`,
-      choices: [`Engineer`, `Intern`, `Manager`],
-    },
-
-    // If Engineer is selected ask for GitHub profile.
-
-    {
-      name: `gitHub`,
-      message: `Please insert your GitHub.`,
-      type: `input`,
-      when: (answers) => {
-        if(answers.role === `Engineer`) {
-          return true;
-        };
+  {
+    name: `name`,
+    message: `Please insert your name.`,
+    type: `input`,
+  },
+  {
+    name: `id`,
+    message: `Please insert your ID.`,
+    type: `input`,
+  },
+  {
+    name: `email`,
+    message: `Please insert your email.`,
+    type: `input`,
+  },
+  {
+    name: `role`,
+    message: `Please select your role.`,
+    type: `list`,
+    choices: [`Engineer`, `Intern`, `Manager`],
+  },
+  // If Engineer is selected ask for GitHub profile.
+  {
+    name: `gitHub`,
+    message: `Please insert your GitHub.`,
+    type: `input`,
+    when: (answers) => {
+      if (answers.role === `Engineer`) {
+        return true;
       }
     },
-
-    // If Intern is selected ask for school.
-
-    {
-      name: `school`,
-      message: `Please insert the name of your school.`,
-      type: `input`,
-      when: (answers) => {
-        if (answers.role === `Intern`) {
-          return true;
-        }
+  },
+  // If Intern is selected ask for school.
+  {
+    name: `school`,
+    message: `Please insert the name of your school.`,
+    type: `input`,
+    when: (answers) => {
+      if (answers.role === `Intern`) {
+        return true;
       }
     },
-
-    // If Manager is selected ask for Office Number
-
-    {
-      name: `officeNumber`,
-      message: `Please insert your office number.`,
-      type: `input`,
-      when: (answers) => {
-        if (answers.role === `Manager`) {
-          return true;
-        }
+  },
+  // If Manager is selected ask for Office Number
+  {
+    name: `officeNumber`,
+    message: `Please insert your office number.`,
+    type: `input`,
+    when: (answers) => {
+      if (answers.role === `Manager`) {
+        return true;
       }
     },
+  },
+  {
+    name: `newMember`,
+    message: `Would you like to add another member?`,
+    type: `confirm`,
+  },
+];
 
+// Question Loop that allows them to add more members
 
-  ];
-;
-;
-
-
-// If Manager is selected ask for Office Number
-
-//         {
-//         name: `officeNumber`,
-//         message: `Please insert your office number.`,
-//         type: `input`,
-//         },
-
-// function generateHTML(answers)
-
-// }
-
-// Function to initialize app
-function init() {
-  inquirer
-  .prompt(questions)
-  .then((answers) =>
-    fs.writeFile(`myteam.html`, generateHTML(answers), (error) => {
-      return error
-        ? console.error(error)
-        : console.log(`Team Profile uploaded successfully!`);
-    })
-  );
+function getAnswers() {
+  return inquirer.prompt(questions).then((answers) => {
+    if (answers.newMember) {
+      return getAnswers();
+    } else {
+      return answers;
+    }
+  });
 }
 
-// Call
-init();
+// Might need to cap at 5 or 6 so that it does not break
+
+getAnswers();
+
+// function createTeamCards() {
+//   inquirer.prompt(questions).then((answers) =>
+//     fs.writeFile("myteam.html", generatedHTML(answers), (error) => {
+//       return error
+//         ? console.error(error)
+//         : console.log("Your team page has been stored successfully!");
+//     })
+//   );
+// }
+
+// createTeamCards();
